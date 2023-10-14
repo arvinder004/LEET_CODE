2df4dbe0bb1908ea28333f010b4427e8671f8b11
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2023-10-14 23:32:11 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,43 @@
+/*Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1*/
+
+bool isPalindrome(int x){
+    long int rev = 0, rem, num = x;
+
+    while(num != 0 && num > 0){
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    } 
+    if(x == rev){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
